f4a04854b1dd5ccf081fa665e7575a5ec9fb20a3
Luci-qq <vladislav2479@gmail.com>
2022-09-08 14:40:48 +0300

    Added matrix class, operators +/+= for mtrx and float values

--- Lab1_1.7.2.cpp
+++ Lab1_1.7.2.cpp
@@ -1,20 +1,170 @@
-﻿// Lab1_1.7.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <vector>
+using std::vector;
 
-#include <iostream>
+class Mtrx
+{
+private:
+	vector<vector<float>> mtrx;
+public:
+
+	void init(const vector<float>& input_values, const int& size_x, const int& size_y)
+	{
+		vector<float> tmp_vector;
+		tmp_vector.resize(size_x);
+		for (size_t y = 0; y < size_y; ++y)
+			mtrx.push_back(tmp_vector);
+		int current_index = 0;
+		for (size_t y = 0; y < size_y; ++y)
+			for (size_t x = 0; x < size_x; ++x)
+			{
+				mtrx[y][x] = input_values[current_index];
+				current_index++;
+			}
+		return;
+	}
+
+	void print_mtrx()
+	{
+		for (int y = 0; y < mtrx.size(); ++y)
+		{
+			for (int x = 0; x < mtrx[y].size(); ++x)
+			{
+				std::cout << mtrx[y][x] << "  ";
+			}
+			std::cout << std::endl;
+		}
+	}
+
+	int get_y_size()
+	{
+		return mtrx.size();
+	}
+
+	int get_x_size()
+	{
+		return mtrx[0].size();
+	}
+
+	float get_elm_by_index(int& x_index, int& y_index)
+	{
+		return mtrx[y_index][x_index];
+	}
+
+	void set_elm_by_index(int& x_index, int& y_index, float& elm)
+	{
+		mtrx[y_index][x_index] = elm;
+		return;
+	}
+};
+
+const Mtrx operator+(Mtrx& left, Mtrx& right)
+{
+	Mtrx result_mtrx;
+	vector<float> result_values;
+	for (int y = 0; y < left.get_y_size(); ++y)
+		for (int x = 0; x < left.get_x_size(); ++x)
+		{
+			result_values.push_back(left.get_elm_by_index(x, y) + right.get_elm_by_index(x, y));
+		}
+	result_mtrx.init(result_values, left.get_x_size(), right.get_y_size());
+	return result_mtrx;
+}
+
+const void operator+=(Mtrx& left, Mtrx& right)
+{
+	for (int y = 0; y < left.get_y_size(); ++y)
+		for (int x = 0; x < left.get_x_size(); ++x)
+		{
+			float set_elm = left.get_elm_by_index(x, y) + right.get_elm_by_index(x, y);
+			left.set_elm_by_index(x, y, set_elm);
+		}
+	return;
+}
+
+const Mtrx operator-(Mtrx& left, Mtrx& right)
+{
+	Mtrx result_mtrx;
+	vector<float> result_values;
+	for (int y = 0; y < left.get_y_size(); ++y)
+		for (int x = 0; x < left.get_x_size(); ++x)
+		{
+			result_values.push_back(left.get_elm_by_index(x, y) - right.get_elm_by_index(x, y));
+		}
+	result_mtrx.init(result_values, left.get_x_size(), right.get_y_size());
+	return result_mtrx;
+}
+
+const void operator-=(Mtrx& left, Mtrx& right)
+{
+	for (int y = 0; y < left.get_y_size(); ++y)
+		for (int x = 0; x < left.get_x_size(); ++x)
+		{
+			float set_elm = left.get_elm_by_index(x, y) - right.get_elm_by_index(x, y);
+			left.set_elm_by_index(x, y, set_elm);
+		}
+	return;
+}
+
+const Mtrx operator+(Mtrx& left, float addition_value)
+{
+	Mtrx result_mtrx;
+	vector<float> result_values;
+	for (int y = 0; y < left.get_y_size(); ++y)
+		for (int x = 0; x < left.get_x_size(); ++x)
+		{
+			result_values.push_back(left.get_elm_by_index(x, y) + addition_value);
+		}
+	result_mtrx.init(result_values, left.get_x_size(), left.get_y_size());
+	return result_mtrx;
+}
+
+const void operator+=(Mtrx& left, float addition_value)
+{
+	for (int y = 0; y < left.get_y_size(); ++y)
+		for (int x = 0; x < left.get_x_size(); ++x)
+		{
+			float set_elm = left.get_elm_by_index(x, y) + addition_value;
+			left.set_elm_by_index(x, y, set_elm);
+		}
+	return;
+}
+
+const Mtrx operator-(Mtrx& left, float difference_value)
+{
+	Mtrx result_mtrx;
+	vector<float> result_values;
+	for (int y = 0; y < left.get_y_size(); ++y)
+		for (int x = 0; x < left.get_x_size(); ++x)
+		{
+			result_values.push_back(left.get_elm_by_index(x, y) - difference_value);
+		}
+	result_mtrx.init(result_values, left.get_x_size(), left.get_y_size());
+	return result_mtrx;
+}
+
+const void operator-=(Mtrx& left, float addition_value)
+{
+	for (int y = 0; y < left.get_y_size(); ++y)
+		for (int x = 0; x < left.get_x_size(); ++x)
+		{
+			float set_elm = left.get_elm_by_index(x, y) - addition_value;
+			left.set_elm_by_index(x, y, set_elm);
+		}
+	return;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	vector<float> test = { 2,3,4,5,1,2,3,4,5,6,7,8 };
+	Mtrx m;
+	m.init(test, 4, 3);
+	Mtrx m1;
+	m1.init(test, 4, 3);
+	m.print_mtrx();
+	std::cout << std::endl;
+	m -= 1000;
+	m.print_mtrx();
+	std::cout << std::endl;
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
